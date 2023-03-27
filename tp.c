071e2be0711d7035b95dc54a6b70a1232b0a9592
dohach1 <114149491+dohach1@users.noreply.github.com>
2023-03-27 11:22:07 +0000

    Update tp.c

--- tp.c
+++ tp.c
@@ -11,15 +11,20 @@ void append (int x){
   node* element = (node*)malloc(sizeof(node));
   element->data=x;
   element->next=NULL;
-  node *current=head;
-  
-  
-  while (current ->next !=NULL){
-    current=current->next;
+  if(*head==NULL){
+    *head=element;
+  else{
+    struct s_node *lastn=*head
+    while (lastn ->next !=NULL){
+      lastn=lastn->next;
+      lastn->=element;
     
 
 
 int main(){
   head=(node*)malloc(sizeof(node));
+  struct node *head=NULL;
+  append(&head,10);
+  
   
   
