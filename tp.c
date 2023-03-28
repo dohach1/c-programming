3594ebe09d728ccd2fe83fd8380bee76dc24d173
dohach1 <114149491+dohach1@users.noreply.github.com>
2023-03-28 00:01:22 +0000

    Update tp.c

--- tp.c
+++ tp.c
@@ -1,4 +1,4 @@
-
+#include<stdio.h>
 #include<stdlib.h>
 
 typedef struct s_node{
@@ -17,14 +17,84 @@ void append (int x){
     struct s_node *lastn=*head
     while (lastn ->next !=NULL){
       lastn=lastn->next;
-      lastn->=element;
+    }
+      lastn->next=element;
     
+void printList(struct node *head)
+{
+    struct node *temp = head;
+    while(temp != NULL)
+    {
+         printf("%d->", temp->data);
+         temp = temp->next;
+    }
+    printf("NULL\n");
+}
 
 
 int main(){
   head=(node*)malloc(sizeof(node));
   struct node *head=NULL;
   append(&head,10);
+  append(&head,100);
+  append(&head,1000);
+  printList(head);
+  return 0;
+}
   
+    //last version 
+    
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+
+void addLast(struct node **head, int x)
+{
+    //create a new node
+    struct node *newNode = malloc(sizeof(struct node));
+    newNode->data =x;
+    newNode->next= NULL;
+    if(*head == NULL)       //if head is NULL, it is an empty list
+         *head = newNode;
+    else                  //find the last node and add the newNode
+    {
+        struct node *lastNode = *head;
+        while(lastNode->next != NULL) 
+        {
+            lastNode = lastNode->next;
+        }
+        lastNode->next = newNode;    //add the newNode at the ende
+    }
+
+}
+
+void printList(struct node *head)
+{
+    struct node *temp = head;
+    while(temp != NULL) //print data
+    {
+         printf("%d->", temp->data);
+         temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main()
+{
+     struct node *head = NULL;
+
+     addLast(&head,10);
+     addLast(&head,100);
+     addLast(&head,1020);
+
+     printList(head);
+
+     return 0;
+}
   
   
